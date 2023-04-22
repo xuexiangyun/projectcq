16ab9382be869301ebc33f092240995aaf4e1896
xuexiangyun <1484751949@qq.com>
2023-04-23 00:55:36 +0800

    主页面制作

---
+++ Assets/Scripts/Camera/MainCameraAct.cs
@@ -0,0 +1,28 @@
+using QFramework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class MainCameraAct : MonoBehaviour
+{
+    private Camera _camera;
+    //
+    private void OnEnable()
+    {
+        _camera = GetComponent<Camera>();
+        var _cameraData = _camera.GetUniversalAdditionalCameraData();
+        var _uiCamera = GameObject.FindGameObjectWithTag("UICamera").GetComponent<Camera>();
+        if(_uiCamera)
+            _cameraData.cameraStack.Add(_uiCamera);
+    }
+
+    private void OnDisable()
+    {
+        var _cameraData = _camera.GetUniversalAdditionalCameraData();
+        var _cameracount = _cameraData.cameraStack.Count;
+        if (_cameraData.cameraStack.Count > 0)
+            _cameraData.cameraStack.RemoveRange(0, _cameracount);
+    }
+}
